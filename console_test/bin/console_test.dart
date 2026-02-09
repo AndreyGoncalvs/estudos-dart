08d4a0f442a6d1fb7abed206884c87f0cc3ecca8
Andrey Gabriel Ferreira Goncalves <02851823230@ifb.local>
2026-02-09 20:58:50 -0300

    Moving project to a separated repository

--- console_test/bin/console_test.dart
+++ console_test/bin/console_test.dart
@@ -1,5 +1,5 @@
 import 'package:console_test/console_test.dart' as console_test;
 
 void main(List<String> arguments) {
-  print('Hello world: ${console_test.multiplica(4.5, -5)}!');
+  print('Hello world: ${console_test.multiplica(4.5, -5.0)}!');
 }
